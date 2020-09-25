025eeee0ee41ebf7aec5e3e443cc3dc3368c9a25
Xmall75 ID <34641833+xmall75@users.noreply.github.com>
2020-09-26 03:00:57 +0700

    [PHP-CLI] Pinterest Image Grabber by Keyword
    Thanks to Abdul Muttaqin for Pinterest Search Image API so that I can make this small tool.

---
+++ pinterest.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Pinterest Image Grabber by Keyword
+** @author Rep - Xmall75
+** Thanks to Abdul Muttaqin for Pinterest Search Image API */
+
+function search($url) {
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, [
+	'Content-Type: application/json'
+	]);
+	$response = curl_exec($curl);
+	curl_close($curl);
+	return $response;
+}
+echo "Pinterest Image Grabber by Keyword\n";
+echo "Keyword : ";
+$input = trim(fgets(STDIN));
+echo "Quantity (Max 45) : ";
+$quantity = trim(fgets(STDIN));
+$keyword = str_replace(" ", "%20", $input);
+$jsondecode = search("http://api.fdci.se/sosmed/rep.php?gambar=$keyword");
+$jsondecode = json_decode($jsondecode, TRUE);
+for ($i = 0; $i <= $quantity-1; $i++) { 
+	$extension = pathinfo($jsondecode[$i], PATHINFO_EXTENSION);
+	echo exec("wget -O Xmall75-Rep-$i.$extension $jsondecode[$i]");
+}
+
+?>
